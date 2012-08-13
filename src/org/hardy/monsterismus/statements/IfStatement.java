13fdee58d76fbec7efdf90fe5e9d27b4125d54b8
Tape <dave@davidkosub.com>
2012-08-13 15:33:22 -0500

    test

--- src/org/hardy/monsterismus/statements/IfStatement.java
+++ src/org/hardy/monsterismus/statements/IfStatement.java
@@ -6,31 +6,28 @@ import org.hardy.monsterismus.statements.conditionals.Conditional;
 import processing.core.PGraphics;
 
 /**
- *
+ * 
  * @author David Kosub
  * @author Jeffery Wooldridge
  * @author Matthew A. Johnston
  * @author Trevor Vardeman
  * @author Carlos Martinez
  */
-public class IfStatement extends ProgrammingStatement
-{
+public class IfStatement extends ProgrammingStatement {
   private static final int FILL_COLOR = 0xFFFF0000;
   private static final int BASE_WIDTH = 300;
   private static final int BASE_HEIGHT = 60;
 
-  public int getColor()
-  {
+  public int getColor() {
     return FILL_COLOR;
   }
 
-  public StatementInstance spawnInstance()
-  {
+  public StatementInstance spawnInstance() {
     return new IfStatementInstance();
   }
 
-  private class IfStatementInstance extends StatementInstance implements Nestable
-  {
+  private class IfStatementInstance extends StatementInstance implements
+      Nestable {
     private Conditional _conditional = Conditional.FOOD_ABOVE;
     private StatementInstance _consequent;
     private StatementInstance _alternative;
@@ -38,82 +35,73 @@ public class IfStatement extends ProgrammingStatement
     private String _label = "If " + _conditional.getLabel() + ", then ";
     private boolean _is_done = true, _is_executed = false;
 
-    public void update(final float $dt)
-    {
+    public void update(final float $dt) {
     }
 
-    public void draw(final PGraphics $graphics)
-    {
+    public void draw(final PGraphics $graphics) {
       $graphics.fill(FILL_COLOR);
       $graphics.rect(_pos.x, _pos.y, BASE_WIDTH, getHeight());
       $graphics.fill(0xFFFFFFFF);
       $graphics.text(_label, _pos.x + 3, _pos.y + 12);
 
-      if(_consequent != null)
-      {
-        $graphics.text("else", _pos.x + 3, _pos.y + _consequent.getHeight() + 35);
+      if (_consequent != null) {
+        $graphics.text("else", _pos.x + 3, _pos.y + _consequent.getHeight()
+            + 35);
         _consequent.draw($graphics);
-      }
-      else
+      } else
         $graphics.text("else", _pos.x + 3, _pos.y + 40);
 
-      if(_alternative != null)
-      {
+      if (_alternative != null) {
         _alternative.draw($graphics);
       }
     }
 
-    public void setPos(final float $x, final float $y)
-    {
+    public void setPos(final float $x, final float $y) {
       super.setPos($x, $y);
 
-      if(_consequent != null)
+      if (_consequent != null)
         _consequent.setPos(_pos.x + 5, _pos.y + 20);
 
-      if(_alternative != null)
-        if(_consequent != null)
-          _alternative.setPos(_pos.x + 5, _pos.y + _consequent.getHeight() + 40);
+      if (_alternative != null)
+        if (_consequent != null)
+          _alternative
+              .setPos(_pos.x + 5, _pos.y + _consequent.getHeight() + 40);
         else
           _alternative.setPos(_pos.x + 5, _pos.y + 45);
     }
 
-    public int getHeight()
-    {
+    public int getHeight() {
       int height = BASE_HEIGHT;
 
-      if(_consequent != null)
-      {
+      if (_consequent != null) {
         height += _consequent.getHeight() - 5;
       }
 
-      if(_alternative != null)
-      {
+      if (_alternative != null) {
         height += _alternative.getHeight() - 10;
       }
 
       return height;
     }
 
-    public int getWidth()
-    {
+    public int getWidth() {
       return BASE_WIDTH;
     }
 
-    public void removeAllInstances(final StatementInstance $instance)
-    {
-      if(_consequent == $instance) _consequent = null;
-      else if(_alternative == $instance) _alternative = null;
+    public void removeAllInstances(final StatementInstance $instance) {
+      if (_consequent == $instance)
+        _consequent = null;
+      else if (_alternative == $instance)
+        _alternative = null;
     }
 
-    public StatementInstance instanceUnder(final float $x, final float $y)
-    {
-      if($x > _pos.x && $y > _pos.y
-        && $x < _pos.x + BASE_WIDTH && $y < _pos.y + getHeight())
-      {
+    public StatementInstance instanceUnder(final float $x, final float $y) {
+      if ($x > _pos.x && $y > _pos.y && $x < _pos.x + BASE_WIDTH
+          && $y < _pos.y + getHeight()) {
         StatementInstance under = null;
-        if(_consequent != null)
+        if (_consequent != null)
           under = _consequent.instanceUnder($x, $y);
-        if(under == null && _alternative != null)
+        if (under == null && _alternative != null)
           under = _alternative.instanceUnder($x, $y);
 
         return under == null ? this : under;
@@ -121,73 +109,62 @@ public class IfStatement extends ProgrammingStatement
       return null;
     }
 
-    public void addChild(StatementInstance $instance)
-    {
+    public void addChild(StatementInstance $instance) {
       $instance.setParent(this);
       int offset = _consequent != null ? _consequent.getHeight() : 10;
 
-      if($instance.getPos().y - _pos.y > offset + 30)
+      if ($instance.getPos().y - _pos.y > offset + 30)
         _alternative = $instance;
       else
         _consequent = $instance;
     }
 
-    public void handleClick()
-    {
-      //Increment the conditional.
+    public void handleClick() {
+      // Increment the conditional.
       _conditional = _conditional.next();
       _label = "If " + _conditional.getLabel() + ", then";
     }
 
-    public boolean isDone()
-    {
+    public boolean isDone() {
       return _is_done;
     }
 
-    public boolean executed()
-    {
+    public boolean executed() {
       return _is_executed;
     }
 
-    public void eval()
-    {
-      //If the action is done, that means we are just now executing it
-      //for the first time.
-      if(_is_done)
-      {
-        //Evaluate the conditional.
+    public void eval() {
+      // If the action is done, that means we are just now executing it
+      // for the first time.
+      if (_is_done) {
+        // Evaluate the conditional.
         _evaluated = _conditional.evaluate() ? _consequent : _alternative;
-        
-        //If the action exists, then begin to execute.
-        if(_evaluated != null)
-        {
+
+        // If the action exists, then begin to execute.
+        if (_evaluated != null) {
           _is_done = false;
           _evaluated.eval();
         }
-        //Otherwise notify that the execution is complete.
+        // Otherwise notify that the execution is complete.
         else
           _is_done = _is_executed = true;
-      }
-      else
-      {
-        //Action finished? reset.
-        if(_evaluated.isDone())
-        {
+      } else {
+        // Action finished? reset.
+        if (_evaluated.isDone()) {
           _evaluated.reset();
           _is_done = _is_executed = true;
         }
-        //Otherwise continue to evaluate.
+        // Otherwise continue to evaluate.
         else
           _evaluated.eval();
       }
     }
 
-    public void reset()
-    {
+    public void reset() {
       _is_done = true;
       _is_executed = false;
 
-      if(_consequent != null)
+      if (_consequent != null)
         _consequent.reset();
     }
   }
