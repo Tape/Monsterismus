32303bddf7a508438a8d6cab328ca086b4e2c97c
Matthew Johnston <warmwaffles@gmail.com>
2012-08-14 23:28:01 -0500

    added comments to the button class

--- src/org/hardy/monsterismus/Button.java
+++ src/org/hardy/monsterismus/Button.java
@@ -5,6 +5,7 @@ import org.hardy.monsterismus.api.Drawable;
 import processing.core.PImage;
 
 /**
+ * A very primitive class that is used to bind events to
  * 
  * @author David Kosub
  * @author Jeffery Wooldridge
@@ -26,6 +27,13 @@ public abstract class Button implements Drawable {
         this.h = h;
     }
 
+    /**
+     * Checks to see if the button was clicked at the given x and y coordinates
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
     public boolean isClicked(int x, int y) {
         clicked = (x < this.w + this.x && x > this.x && y < this.h + this.y && y > this.y);
         if (clicked && run != null) {
@@ -34,14 +42,25 @@ public abstract class Button implements Drawable {
         return clicked;
     }
 
+    /**
+     * Releases the button and forces it to be unclicked
+     */
     public void release() {
         clicked = false;
     }
 
+    /**
+     * Bind an event to the Button, so that when clicked, it will be run.
+     * 
+     * @param r the runnable class or instance that you want to have execute
+     */
     public void event(Runnable r) {
         this.run = r;
     }
 
+    /**
+     * Updates the button
+     */
     public void update(final float $dt) {
     }
 }
